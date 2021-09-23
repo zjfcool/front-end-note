85be7502e8a0c32cff01ebfd2545cc0aaf0b3d03
zjfcool <2568696196@qq.com>
2021-09-23 09:19:11 +0800

    feat:two-num

---
+++ algorithm/two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
+function twoNum(nums, target) {
+    let ret = []
+    for (let i = 0; i < nums.length; i++) {
+        const a = nums[i], b = target - a;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === b) ret.push([i, j])
+        }
+    }
+    return ret;
+}
+console.log(twoNum([2, 7, 11, 15], 9))
+console.log(twoNum([3, 2, 4], 6))
+console.log(twoNum([3, 3, 2, 4, 3], 6))
